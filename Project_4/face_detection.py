58b00f13d07502e529528671d7845e5783a2fcec
Sukanta Bera <berasukanta02@gmail.com>
2026-02-20 13:43:05 +0530

    Update face_detection.py

--- Project_4/face_detection.py
+++ Project_4/face_detection.py
@@ -1,72 +1,66 @@
-# Face Detection Project using OpenCV
-# OpenCV = Open Source Computer Vision Library
-
+# FACE DETACTION PROJECTS USING OPENCV(COMPUTER VISION)
+#OpenCV = Open Source Computer Vision Library
 
 import cv2
-
-
-# Load Pre-trained Haar Cascade Model
 
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
+#START WEBCAM
 
-# Start Webcam
+# For windows only cap = cv2.VideoCapture(0)
+# For mac only cap = cv2.VideoCapture(2, cv2.CAP_AVFOUNDATION)
 
-cap = cv2.VideoCapture(2, cv2.CAP_AVFOUNDATION)
+cap = cv2.VideoCapture(0)
 
-# Check if webcam opened successfully
 if not cap.isOpened():
-    print("Error: Cannot access webcam")
+    print("Error: Cannot access webcam.")
     exit()
 
-print("Press 'q' to quit")
-print("Press 's' to save image")
+print("Press 'q' to quit.")
+print("Press 's' to save a image.")
 
-
-
-# Main Loop
+#main loop
 
 while True:
     ret, frame = cap.read()
-
     if not ret:
-        print("Failed to grab frame")
+        print("Failed to grab frame.")
         break
 
-    # Convert to grayscale
+        #convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces
+        #detect faces
     faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.3,
+        gray, 
+        scaleFactor=1.3, 
         minNeighbors=5
-    )
+        )
 
-    # Draw rectangle around faces
+#draw rectangles around faces
     for (x, y, w, h) in faces:
         cv2.rectangle(
-            frame,
-            (x, y),
-            (x + w, y + h),
-            (0, 255, 0),
+            frame, 
+            (x, y), 
+            (x + w, y + h), 
+            (0,255,0), 
             2
         )
 
-    # Display face count
+#display face count
     cv2.putText(
-        frame,
-        f"Faces Detected: {len(faces)}",
-        (10, 30),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.8,
-        (0, 255, 0),
+        frame, 
+        f"Faces: {len(faces)}", 
+        (10, 30), 
+        cv2.FONT_HERSHEY_SIMPLEX, 
+        1, 
+        (0,255,0), 
         2
     )
 
-    # Show output window
+     # Show output window
     cv2.imshow("Face Detection System", frame)
 
     key = cv2.waitKey(1) & 0xFF
